ee426c6d5ebb7bd1ebf08939028a40cd631ae07c
Luka <lvilaj@gsma.com>
2020-04-30 16:27:21 +0100

    Initial Commit

---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Luka Vilaj on 30/04/2020.
+//  Copyright © 2020 Luka Vilaj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
